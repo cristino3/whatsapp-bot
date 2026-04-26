ccda3fde8b02d16620a6af2d87dd8a731e7f53aa
Cristi Cosovan <cristicosovan@gmail.com>
2026-04-27 02:44:55 +0300

    Update bot.js
    fix puppeteer render

--- bot.js
+++ bot.js
@@ -19,10 +19,14 @@ app.listen(process.env.PORT || 3000, () => {
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
-});
+    headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu'
+    ]
+}
 
 const GROUPS = [
     "Mihai Bravu",
